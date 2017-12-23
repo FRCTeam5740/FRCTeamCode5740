eeeb45751d7593d6d1c04faf470daf8afd8c20cd
lukecrum20 <lukecrum20@gmail.com>
2017-12-23 13:25:00 -0500

    edit code

--- DemoBotCode/src/org/usfirst/frc/team5740/robot/Robot.java
+++ DemoBotCode/src/org/usfirst/frc/team5740/robot/Robot.java
@@ -4,15 +4,16 @@ package org.usfirst.frc.team5740.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.TalonSRX;
 public class Robot extends IterativeRobot {
 	TalonSRX t0 = new TalonSRX(0);
 	TalonSRX t1 = new TalonSRX(1);
 	TalonSRX t2 = new TalonSRX(2);
 	TalonSRX t3 = new TalonSRX(3);
-	Joystick joystick = new Joystick(0);
-
-
+	Joystick jsL = new Joystick(0);
+	Joystick jsR = new Joystick(1);
+	RobotDrive drive = new RobotDrive(t0, t1, t2, t3);
 	@Override
 	public void robotInit() {
 
@@ -47,7 +48,7 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		
+		drive.tankDrive(-jsL.getY(), -jsR.getY());
 	}
 
 
