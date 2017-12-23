47e7ea74b932c72865be3969a550db492c0052fb
lukecrum20 <lukecrum20@gmail.com>
2017-12-23 14:29:15 -0500

    stop using TankDrive and wrote my own drive system

--- DemoBotCode/src/org/usfirst/frc/team5740/robot/Robot.java
+++ DemoBotCode/src/org/usfirst/frc/team5740/robot/Robot.java
@@ -6,13 +6,15 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.XboxController;
 public class Robot extends IterativeRobot {
 	TalonSRX right = new TalonSRX(0); //right side motors
 	TalonSRX left = new TalonSRX(1); //left side motors
 
-	Joystick jsL = new Joystick(0); //left joystick
-	Joystick jsR = new Joystick(1); //right joystick
+	//Joystick jsL = new Joystick(0); //left joystick
+	//Joystick jsR = new Joystick(1); //right joystick
 	RobotDrive drive = new RobotDrive(right, left); //define robot drive
+	Joystick controller = new Joystick(1);
 	@Override
 	public void robotInit() {
 
@@ -47,7 +49,21 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		drive.tankDrive(-jsL.getY(), -jsR.getY()); //drive
+		drive.tankDrive(-controller.getY(), -controller.getX()); //drive
+		double LeftX = controller.getRawAxis(1);
+		double LeftY = controller.getRawAxis(2);
+		double RightX = controller.getRawAxis(4);
+		double RightY = controller.getRawAxis(1);
+		if(LeftY != 0) {
+			drive.drive(LeftX, 0);
+		}
+		if(RightX != 0) {
+			if(RightX < 0) {
+				drive.drive(RightX, RightX);
+			} else {
+				drive.drive(RightX, -RightX);
+			}
+		}
 	}
 
 
